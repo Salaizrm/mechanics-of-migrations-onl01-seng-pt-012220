c6f592f74777d4448186f14fbfd3aa2b82799bbb
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-31 22:40:11 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -9,6 +9,17 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     end
   end
 
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS artists (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      genre TEXT,
+      age INTEGER,
+      hometown TEXT
+    )
+  SQL
+
+
   ActiveRecord::Base.connection.execute(sql)
 
 
